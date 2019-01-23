c8644283ea2862de57c46468e67dcd361bf91420
htzs12 <www.htzs@qq.com>
2019-01-24 07:23:49 +0800

    update-6

--- apps/goods/serializes.py
+++ apps/goods/serializes.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from .models import Goods
 
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True,max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+# class GoodsSerializer(serializers.Serializer):
+#     name = serializers.CharField(required=True,max_length=100)
+#     click_num = serializers.IntegerField(default=0)
+#     goods_front_image = serializers.ImageField()
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = '__all__'
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -6,6 +6,8 @@ from .models import Goods
 from .serializes import GoodsSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
 class GoodsListView(APIView):
@@ -13,3 +15,14 @@ class GoodsListView(APIView):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods,many=True)
         return Response(goods_serializer.data)
+
+    # def post(self,request):
+    #     serializer = GoodsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data,status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_CREATED)
+
+
+class GoodsList1View(mixins.ListModelMixin,generics.GenericAPIView):
+    pass
